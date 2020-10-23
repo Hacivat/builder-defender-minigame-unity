5aad47488fbce555fa71717626d473d07829329b
Osman Pekaydın <pekaydin580@gmail.com>
2020-10-23 03:01:45 +0300

    Initial commit

---
+++ Assets/Scripts/ResourceManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    public static ResourceManager Instance { get; private set; }
+    private Dictionary<ResourceTypeSO, int> resourceAmountDictionary;
+    private ResourceTypeListSO resourceTypeList;
+
+    private void Awake() {
+        Instance = this;
+        resourceAmountDictionary = new Dictionary<ResourceTypeSO, int>();
+
+        resourceTypeList = Resources.Load<ResourceTypeListSO>(typeof(ResourceTypeListSO).Name);
+        foreach (ResourceTypeSO resourceType in resourceTypeList.list) {
+            resourceAmountDictionary[resourceType] = 0;
+        }
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.T)) {
+            AddResource(resourceTypeList.list[0], 1);
+            TestResourceAmountDictionary();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y)) {
+            AddResource(resourceTypeList.list[1], 1);
+            TestResourceAmountDictionary();
+        }
+    }
+
+    public void TestResourceAmountDictionary() {
+        foreach (ResourceTypeSO resourceType in resourceAmountDictionary.Keys) {
+            Debug.Log(resourceType.nameString + ": " + resourceAmountDictionary[resourceType]);
+        }
+    }
+
+    public void AddResource(ResourceTypeSO resourceType, int amount) {
+        resourceAmountDictionary[resourceType] += amount;        
+    }
+}
